84fda89d1ac7a1bcd466302b0e9b759389768ead
Çağatay Yıldız <cagatay.yildiz@yakintech.com>
2017-07-27 03:32:56 +0300

    Add project files.

---
+++ SingletonPattern/DBHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class DBHelper
+    {
+        private DBHelper(string connectionstring)
+        {
+
+        }
+
+        private static DBHelper _helper;
+
+        public static DBHelper CreateDbHelper(string connectionstring)
+        {
+            if (_helper == null)
+            {
+                _helper = new DBHelper(connectionstring);
+            }
+
+            return _helper;
+        }
+        public string ConnectionString { get; set; }
+
+        public string StateMessage { get; set; }
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBHelper helper1 = DBHelper.CreateDbHelper("con 1");
+            helper1.StateMessage = "deneme";
+
+            DBHelper helper2 = DBHelper.CreateDbHelper("con 2");
+
+            Console.WriteLine(helper2.StateMessage);
+            Console.ReadKey();
+        }
+    }
+}
